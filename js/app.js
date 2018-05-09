4565c0037b6b783e5e81b0073a2b86ad1c54685c
Vlad Sivirin <sivirin@gmail.com>
2018-05-09 16:30:26 +0300

    Updated Start to ES6 class

--- js/app.js
+++ js/app.js
@@ -353,40 +353,44 @@ const Game = function() {
 const game = new Game();
 
 // start/stop  button
-const Start = function() {
-    this.e = document.querySelector('.stopped');
-    this.e.addEventListener('click', () => {
-        this.isStarted();
-    });
-}
+class Start {
+    constructor() {
+        this.e = document.querySelector('.stopped');
+        this.e.addEventListener('click', () => {
+            this.isStarted();
+        });
 
-Start.prototype.isStarted = function() {
-    this.e.classList.remove('stopped');
-    this.e.classList.add('started');
-    this.e.textContent = 'Stop';
-    allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
-    allItems = [item1, item2, item3, item4, item5, item6];
-    modal.counter = 0;
-    this.e = document.querySelector('.started');
-    this.e.addEventListener('click', () => {
-        this.isStopped();
-    });
-}
+        this.isStarted = () => {
+            this.e.classList.remove('stopped');
+            this.e.classList.add('started');
+            this.e.textContent = 'Stop';
+            allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+            allItems = [item1, item2, item3, item4, item5, item6];
+            modal.counter = 0;
+            this.e = document.querySelector('.started');
+            this.e.addEventListener('click', () => {
+                this.isStopped();
+            });
+        }
 
-Start.prototype.isStopped = function() {
-    this.e.classList.add('stopped');
-    this.e.classList.remove('started');
-    this.e.textContent = 'Start';
-    allEnemies = [];
-    allItems = [];
-    // reset score
-    modal.counter = 0;
-    game.score.textContent = `Your score: ${modal.counter}`;
-    modal.score.textContent = `Your score: ${modal.counter}`;
-    this.e = document.querySelector('.stopped');
-    this.e.addEventListener('click', () => {
-        this.isStarted();
-    });
+        this.isStopped = function() {
+            this.e.classList.add('stopped');
+            this.e.classList.remove('started');
+            this.e.textContent = 'Start';
+            player.x = player.initX;
+            player.y = player.initY;
+            allEnemies = [];
+            allItems = [];
+            // reset score
+            modal.counter = 0;
+            game.score.textContent = `Your score: ${modal.counter}`;
+            modal.score.textContent = `Your score: ${modal.counter}`;
+            this.e = document.querySelector('.stopped');
+            this.e.addEventListener('click', () => {
+                this.isStarted();
+            });
+        }
+    }
 }
 
 const start = new Start();
